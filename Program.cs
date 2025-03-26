0644ac27b1d31e46550916624304687ee3931709
Annette Jensen <annetteduedal@gmail.com>
2025-03-26 10:25:50 +0100

    dummies

--- Program.cs
+++ Program.cs
@@ -12,8 +12,23 @@ namespace Genspil
         public static void Main(string[] args)
         {
 
+            // Deklarér boardGames som et statisk felt
+        private static List<BoardGame> boardGames;
 
-           
+        public static void Main(string[] args)
+        {
+            boardGames = new List<BoardGame>
+            {
+                new BoardGame("Catan", "Standard", "Strategy", 3, 4, "English"),
+                new BoardGame("Ticket to Ride", "Deluxe", "Family", 2, 5, "English")
+            };
+
+            // Tilføj nogle produkter til hvert boardgame
+            boardGames[0].AddProduct(new Product(1, "God", 75));
+            boardGames[0].AddProduct(new Product(2, "Okay", 40));
+            boardGames[1].AddProduct(new Product(3, "God", 75));
+            boardGames[1].AddProduct(new Product(4, "God", 75));
+
         }
 
 
