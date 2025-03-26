9d6e9806b110572296f9956d0e4a21ee994764d6
Khadra Khalif <kamk71385@edu.ucl.dk>
2025-03-26 09:09:59 +0100

    whatever

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Product p1 = new Product(10, "reperation", 200.0);
+
+            Console.WriteLine($"Produkt detaljer {p1}");
+            p1.sell();
+            p1.editProduct(100.0, "på lager");
+            Console.WriteLine($"{p1} er nu opdateret");
+            p1.sell();
+            Console.ReadLine(); 
         }
     }
 }
